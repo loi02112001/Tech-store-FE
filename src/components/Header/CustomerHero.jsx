7055daf5d0631f7cf1c6f7a95e9e98fc0f8218d1
loi123 <loi65732102@gmail.com>
2024-05-08 22:18:30 +0700

    Add react carousel

--- src/components/Header/CustomerHero.jsx
+++ src/components/Header/CustomerHero.jsx
@@ -1,45 +1,37 @@
-// import { useEffect, useRef } from "react"
-// import TinySlider from "tiny-slider-react"
-// import "tiny-slider/dist/tiny-slider.css"
+import Slider from 'react-slick'
 
-// import "./CustomerHero.css"
+import './CustomerHero.css'
 
-// const CustomerHero = () => {
-//   const settings = {
-//     lazyload: true,
-//     nav: false,
-//     mouseDrag: true,
-//     controls: false,
-//     slideBy: "page",
-//     swipeAngle: false,
-//     autoplay: true,
-//     speed: 1000,
-//     autoplayTimeout: 5000,
-//   }
-//   const images = [
-//     "https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png",
-//     "https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png",
-//     "https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png",
-//     "https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png",
-//     "https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png",
-//     "https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png",
-//   ]
+const CustomerHero = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    cssEase: 'linear'
+  }
+  const images = [
+    'https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png',
+    'https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png',
+    'https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png',
+    'https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png',
+    'https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png',
+    'https://file.hstatic.net/200000722513/file/layout_thang_4_1015x325_copy_0a4cd291bc5b4fb2a227e40d9a7e6319.png'
+  ]
 
-//   const sliderRef = useRef(null)
-
-//   useEffect(() => {
-//     const slider = sliderRef.current
-//     return () => slider && slider.destroy()
-//   }, [])
-
-//   return (
-//     <TinySlider settings={settings} ref={sliderRef}>
-//       {images.map((el, index) => (
-//         <div key={index}>
-//           <img className="w-full aspect-[3/1]" src={el} alt="" />
-//         </div>
-//       ))}
-//     </TinySlider>
-//   )
-// }
-// export default CustomerHero
+  return (
+    <div className="overflow-hidden">
+      <Slider {...settings}>
+        {images.map((el, index) => (
+          <div key={index}>
+            <img className="w-full aspect-[3/1]" src={el} alt="" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  )
+}
+export default CustomerHero
--- src/pages/HomePage/CustomerHomePage.jsx
+++ src/pages/HomePage/CustomerHomePage.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom'
 
 import CustomerHeader from '@/components/Header/CustomerHeader'
-// import CustomerHero from "@/components/Header/CustomerHero"
+import CustomerHero from '@/components/Header/CustomerHero'
 
 function CustomerHomePage() {
   return (
     <>
       <CustomerHeader />
       <Outlet />
-      {/* <CustomerHero /> */}
+      <CustomerHero />
     </>
   )
 }
